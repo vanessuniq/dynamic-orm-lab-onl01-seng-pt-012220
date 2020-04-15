bdb4590ca4d5520209af119c944e55886a9805bf
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-15 08:26:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -6,7 +6,7 @@ class InteractiveRecord
 
     def self.table_name
         self.to_s.downcase.pluralize
-      end
+    end
     
       def self.column_names
         DB[:conn].results_as_hash = true
