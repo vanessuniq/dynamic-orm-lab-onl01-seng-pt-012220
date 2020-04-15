14306f45808ae9e0230f8b981e8594c0830c62ae
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-15 08:28:03 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,14 +8,14 @@ class InteractiveRecord
       self.to_s.downcase.pluralize
     end
     
-      def self.column_names
-        DB[:conn].results_as_hash = true
+    def self.column_names
+      DB[:conn].results_as_hash = true
     
-        table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name})")
-        column_names = []
-    
-        table_columns.each do |col|
-          column_names << col["name"]
+      table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name})")
+      column_names = []
+  
+      table_columns.each do |col|
+        column_names << col["name"]
         end
     
         column_names.compact
